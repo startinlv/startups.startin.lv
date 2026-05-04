d183653f57e4022c52129a6b7bcfcfae3d68f91d
ringoldsdev <ringoldsdev@users.noreply.github.com>
2026-05-04 16:27:21 +0000

    deploy: startinlv/private.startups.startin.lv@23537c1e121a2f0d2f3f560e494513afc7fa053d

---
+++ _app/immutable/chunks/stores.B0hu4Zaq.js
@@ -0,0 +1,1 @@
+import{s as r}from"./entry.E5L9lsIn.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},i={subscribe(s){return e().navigating.subscribe(s)}};export{i as n,a as p};
---
+++ _app/immutable/entry/start.CgSDH7ZI.js
@@ -0,0 +1,1 @@
+import{f as a}from"../chunks/entry.E5L9lsIn.js";export{a as start};
