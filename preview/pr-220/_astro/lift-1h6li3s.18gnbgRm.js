e081fb4ee16a37e66958a68c7c400b036f749b78
startin-deploy <deploy@startups.startin.lv>
2026-09-21 19:12:24 +0000

    preview pr-220: removed

--- preview/pr-220/_astro/lift-1h6li3s.18gnbgRm.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./datasets.SJVN82Oz.js";var n=e,r={baseline:t},i={x:({}={})=>(e=>e.x??`employees_gp`),y:({}={})=>(e=>e.y??`turnover`),groupBy:({}={})=>(e=>e.groupBy)};export{r as atoms,i as callbacks,n as dataset};
