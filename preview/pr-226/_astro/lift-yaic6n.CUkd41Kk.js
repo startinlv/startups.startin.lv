99dd8d1cfd4b948fb174bcf7c2ef00fbdec77627
startin-deploy <deploy@startups.startin.lv>
2026-09-22 06:22:28 +0000

    preview pr-226: removed

--- preview/pr-226/_astro/lift-yaic6n.CUkd41Kk.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./t.OkQdCvWW.js";import{l as t,o as n}from"./format.BZ7BMWYX.js";import{a as r}from"./datasets.SJVN82Oz.js";var i=r,a={},o={value:({}={})=>(e=>e.map(e=>n(e.kpi.total_profit))),share:({}={})=>(n=>n.map(n=>e(`metric.shareOfTotal`,{percent:t(n.share.profit_share_percent)})))};export{a as atoms,o as callbacks,i as dataset};
